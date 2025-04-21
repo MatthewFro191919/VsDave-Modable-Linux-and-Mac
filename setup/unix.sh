11315ab09955b5d0c70b3e1282747d9eb5c2af87
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-21 08:51:47 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -19,6 +19,7 @@ haxelib install actuate 1.9.0
 haxelib install hscript
 haxelib install hxCodec 2.5.1          
 haxelib install linc_luajit
+haxelib install format
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git faxe https://github.com/uhrobots/faxe
 haxelib install hxcpp-debug-server
